49f0c75dba0999caa43ee193982c5c0e80824c4b
Javier Venegas <venegas10@gmail.com>
2014-01-14 21:19:05 -0800

    Layout subviews
    Flag to layout the subview inside of a viewController view.
    
    Add a new view controller with a table view and a toolbar.

--- GTScrollNavigationBar/GTScrollNavigationBar.h
+++ GTScrollNavigationBar/GTScrollNavigationBar.h
@@ -18,6 +18,7 @@ typedef enum {
 
 @property (strong, nonatomic) UIScrollView *scrollView;
 @property (assign, nonatomic) GTScrollNavigationBarState scrollState;
+@property (assign, nonatomic) BOOL keepSubviewsLayout;
 
 - (void)resetToDefaultPosition:(BOOL)animated;
 
