29e850fafe0997fd331e0ff6b2520b8f9cec3b12
Luu Gia Thuy <luugiathuy@gmail.com>
2015-07-26 23:13:39 +0800

    Add Podfile and Test target to Example project

---
+++ GTScrollNavigationBarExample/GTScrollNavigationBarTests/GTScrollNavigationBarSpec.m
@@ -0,0 +1,28 @@
+//
+//  GTScrollNavigationBarSpec.m
+//  GTScrollNavigationBarExample
+//
+//  Created by Luu Gia Thuy on 7/26/15.
+//  Copyright (c) 2015 Luu Gia Thuy. All rights reserved.
+//
+
+#import <Specta/Specta.h>
+#import <KIF/KIF.h>
+
+SpecBegin(GTScrollNavigationBar)
+
+describe(@"GTScrollNavigationBar", ^{
+    beforeAll(^{
+    });
+    
+    beforeEach(^{
+    });
+    
+    afterEach(^{
+    });
+    
+    afterAll(^{
+    });
+});
+
+SpecEnd
